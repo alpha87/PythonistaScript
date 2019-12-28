3ee148ce33d9df71c13d8642c467dbdf8bc3ea1f
alpha87 <jianxun2004@gmail.com>
2019-12-28 20:11:50 +0800

    支持中文

--- installScript.py
+++ installScript.py
@@ -12,7 +12,6 @@ file_url = input("请输入需要安装的脚本链接：")
 response = requests.get(file_url).text
 file_name = re.findall(r'FILE_NAME: "(.*?)"', response)[0]
 
-with open(file_name, "w") as f:
+with open(file_name, "w", encoding="utf-8") as f:
     f.write(response)
-
 print("安装完成！")
