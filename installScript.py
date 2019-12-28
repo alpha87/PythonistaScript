b1268c0a575503c4d86d13fe938a8e4af79f4db7
alpha87 <jianxun2004@gmail.com>
2019-12-28 20:01:46 +0800

    Python 脚本安装程序

---
+++ installScript.py
@@ -0,0 +1,18 @@
+# ------------------------------- #
+#  FILE_NAME: "installScript.py"  #
+#  VERSION: "v1.0"                #
+#  Desc: Python Script Installer  #
+# ------------------------------- #
+
+import re
+import requests
+
+
+file_url = input("请输入需要安装的脚本链接：")
+response = requests.get(file_url).text
+file_name = re.findall(r'FILE_NAME: "(.*?)"', response)[0]
+
+with open(file_name, "w") as f:
+    f.write(response)
+
+print("安装完成！")
